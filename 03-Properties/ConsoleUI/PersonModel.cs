869b0fd0d6db84dcf2693c954480c6cb9c894aa9
Joshhortt <joshhortt@yahoo.com>
2021-03-03 11:55:25 +0000

    Add equivalent property type of 'Age' with if' else' age range condition.

--- 03-Properties/ConsoleUI/PersonModel.cs
+++ 03-Properties/ConsoleUI/PersonModel.cs
@@ -10,6 +10,30 @@ namespace ConsoleUI
     {
 		public int FirstName { get; set; }
 		public string LastName { get; set; }
-		public int Age { get; set; }
+		//public int Age { get; set; }
+
+
+	//  the code below is the equivalent as the commented line property of 'Age'.
+		private int _age; // private backing field of creating variables (underscore + camelCase)
+
+			public int Age
+		{
+			get 
+			{
+				return _age; 
+			}
+			set 
+			{ 
+				if(value >= 0 && value < 126)
+				{
+					_age = value;
+				}
+				else
+				{
+					throw new ArgumentOutOfRangeException("value, Age needs to be in a valid range. ");
+				}
+			}
+		}
+
 	}
 }
