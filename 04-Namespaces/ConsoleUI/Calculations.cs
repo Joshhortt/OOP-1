b1ff0eb75d1267f650dfb775c42113bcb765d483
Joshhortt <joshhortt@yahoo.com>
2021-03-03 14:07:41 +0000

    First commit. Add one class  and another class inside directory. Add correct way of writing namespaces.

---
+++ 04-Namespaces/ConsoleUI/Calculations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class Calculations  // static Calculations class
+    {
+        public static double Add(double x, double y)  // Method
+        {
+            return x + y;
+        }
+    }
+}
---
+++ 04-Namespaces/ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿// Object Oriented Programming - part 1 - Namespaces
+using System;
+using System.Collections.Generic;
+using ConsoleUI.Models;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new List<PersonModel>();  // List Instance
+            Console.WriteLine();
+
+            PersonModel person = new PersonModel(); // person Instance
+
+            Calculations.Add(4, 3);
+
+            Console.ReadLine();
+        }
+    }
+}
