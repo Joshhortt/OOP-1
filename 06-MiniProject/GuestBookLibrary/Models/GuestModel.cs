922d4f2c984c1c991f9b96cea33b4f5ef77cc1c3
Joshhortt <joshhortt@yahoo.com>
2021-03-04 14:03:45 +0000

    Fc. Add Mini-project. Add class Library, add new directory with another class within.

---
+++ 06-MiniProject/GuestBookLibrary/Models/GuestModel.cs
@@ -0,0 +1,15 @@
+﻿// Object Oriented Programming - part 1 - Mini-Project
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBookLibrary.Models
+{
+    public class GuestModel
+    {
+       
+    }
+}
